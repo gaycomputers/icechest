1579f6ce8239b9534607195b85ad5730a3d59f9f
Josh <trismix@protonmail.com>
2019-04-14 13:27:03 -0400

    receives http correctly

--- src/HttpRequest.java
+++ src/HttpRequest.java
@@ -1,88 +1,150 @@
+//it would be unethical for me to not site this article, https://www.baeldung.com/java-http-request which helped me understand a better method of sending HTTP than my original socket method
 package icechest;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HttpRequest{
+public class HttpRequest {
     private int port = 80;
     private String host = null;
-    private ArrayList<String> lines = new ArrayList<String>();
+    private ArrayList<String[]> lines = new ArrayList<String[]>();
     private ArrayList<String> responseHeader = new ArrayList<String>();
     private ArrayList<String> responseBody = new ArrayList<String>();
-    public HttpRequest(){
+    private boolean drop = false;
+    private StringBuffer response = null;
+
+    public HttpRequest() {
 
     }
-    public void setHost(String hos){
+
+    public void drop() {
+        drop = true;
+    }
+
+    public void setHost(String hos) {
         host = hos;
     }
-    public void setPort(int por){
+
+    public void setPort(int por) {
         port = por;
     }
-    public String getHost(){
+
+    public String getHost() {
         return host;
     }
-    public int getPort(){
+
+    public int getPort() {
         return port;
     }
-    public void output(){
+
+    public void output() {
         System.out.println(lines);
     }
-    public void getReturn(BufferedReader in){
-        
-        System.out.println("Writing return to object: \r\n");
-        try{
-            String line = "";
-            line = in.readLine();
-            if(line != null){
-            while (!(line.isEmpty())) {
-                    System.out.println(line);
-                    responseHeader.add(line);
-                    line = in.readLine();
-            }
-            line = in.readLine();
-            while (!line.isEmpty()){
-                System.out.println(line);
-                responseBody.add(line);
-                line = in.readLine();
-            }
+
+    public void getResponse(HttpURLConnection httpcon) throws IOException{
+        int status = httpcon.getResponseCode();
+        BufferedReader in = new BufferedReader(new InputStreamReader(httpcon.getInputStream())); //thanks, google
+        response = new StringBuffer();
+        String line;
+        while((line = in.readLine()) != null){
+            response.append(line);
         }
-        }
-        catch(IOException i){
-            //donothing
-            System.out.println(i);
-        }
-        System.out.println("DONE");
-        System.out.println(responseBody);
-    }  
-    public void addLine(String line){
+        in.close();
+        System.out.println(response);
+    }
+
+    public void addLine(String[] line) {
         lines.add(line);
     }
-    public void sendResponse(Socket socket) throws IOException{
+
+    public void sendResponse(Socket socket) throws IOException {
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
         System.out.println("Sending to client: \r\n");
-        for(int i = 0; i < responseHeader.size(); i++){
+        for (int i = 0; i < responseHeader.size(); i++) {
             System.out.println(responseHeader.get(i));
             out.write(responseHeader.get(i) + "\r\n");
         }
         out.write("\r\n");
-        for(int i = 0; i < responseBody.size(); i++){
+        for (int i = 0; i < responseBody.size(); i++) {
             System.out.println(responseBody.get(i));
             out.write(responseBody.get(i) + "\r\n");
         }
         out.flush();
     }
-    public void sendRequest() throws IOException{
-        Socket socket = new Socket(host, port);
-        //socket.getInputStream.read();
-        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+    private String buildHttpString() throws UnsupportedEncodingException{
+        StringBuilder message = new StringBuilder();
         for(int i = 0; i < lines.size(); i++){
-            out.write(lines.get(i) + "\r\n");
+            message.append(URLEncoder.encode(lines.get(i)[0], "UTF-8"));
+            message.append("=");
+            for(int j = 1; j < (lines.get(i)).length; j++){
+                message.append(lines.get(i)[j]);
+                if(lines.get(i).length >= j++){
+                    message.append("");
+                }
+            }
+            message.append("&");
         }
-        out.flush();
-        getReturn(in);
-        socket.close();
+        String messageString = message.toString();
+        System.out.println("TESTINGBUELDER");
+        if(messageString.length() > 0){
+            System.out.println(messageString.substring(0, messageString.length() - 1));
+            return messageString.substring(0, messageString.length() - 1);
+        }
+        else{
+            System.out.println(messageString);
+            return messageString;
+        }
+    }
+
+    private void sendHTTP() {
+        try {
+            URL url = new URL("http://"+host);
+            HttpURLConnection httpcon = (HttpURLConnection) url.openConnection();
+            httpcon.setDoOutput(true);
+            DataOutputStream out = new DataOutputStream(httpcon.getOutputStream());
+            out.writeBytes(buildHttpString());
+            out.flush();
+            out.close();
+            getResponse(httpcon);
+            httpcon.disconnect();
+        } catch (MalformedURLException m) {
+            System.out.println(m);
+        } catch (IOException i){
+            System.out.println(i);
+        }
+    }
+
+    private void doHTTPS() {
+
+        // blindly use sockets
+
+        /*
+         * old helpful code Socket socket = new Socket(host, port);
+         * //socket.getInputStream.read(); BufferedWriter out = new BufferedWriter(new
+         * OutputStreamWriter(socket.getOutputStream(), "UTF8")); BufferedReader in =
+         * new BufferedReader(new InputStreamReader(socket.getInputStream())); for(int i
+         * = 0; i < lines.size(); i++){ System.out.println(lines.get(i));
+         * out.write(lines.get(i) + "\r\n"); } out.write(""); out.flush();
+         * getReturn(in); socket.close();
+         */
+    }
+
+    public void sendRequest() throws IOException {
+        System.out.println(lines);
+        if (!drop) {
+            System.out.println(host);
+            if (port == 80) {
+                sendHTTP();
+            } else if (port == 443) {
+                doHTTPS();
+            }
+        }
     }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -17,36 +17,55 @@ public class Parser{ //takes a string from input buffer and parses what we want 
     }
 
     private String[] splitter(String line, String delimeters){
-        String[] splitWords = line.split(delimeters);
-        return splitWords;
+        try{
+            String[] splitWords = line.split(delimeters);
+            return splitWords;
+        }
+        catch(NullPointerException e){
+            System.out.println(e);
+            String[] splitWords = {};
+            return splitWords;
+        }
     }
     public HttpRequest parse(BufferedReader reader, ServerSocket server){
         HttpRequest httprequest = new HttpRequest();
+        boolean parsing = true;
         do{
             try{
                line = reader.readLine();//iterates http requesttry
+            if (line != null && line.length() > 0){
                 String[] words = splitter(line, "[ ]+");
-                if(words[0].equals("Accept-Encoding:")){ //skip gzip encoding, etc., otherwise you're viable to get binary back
-                    line = reader.readLine();
-                    words = splitter(line, "[ ]+");
-                }
-                if (words[0].equals("Host:")){ //parse out host so we know where to open a socket
-                    words = splitter(words[1], "[:]");
-                    httprequest.setHost(words[0]);
-                    if(words.length > 1){
-                        int port = Integer.parseInt(words[1]);
-                        httprequest.setPort(port);
+                    boolean uglyword = true;
+                    while(uglyword){
+                        if (words[0].equals("Accept-Encoding:")) { // skip gzip encoding, etc., otherwise you're liable to get binary
+                            line = reader.readLine();
+                            words = splitter(line, "[ ]+");//drive loop
+                        } else if (words[0].equals("Host:")) { // parse out host so we know where to open a socket
+                            if (words[1] == "detectportal.firefox.com") {
+                                httprequest.drop();
+                            }
+                            words = splitter(words[1], "[:]");
+                            httprequest.setHost(words[0]);
+                            if (words.length > 1) {
+                                int port = Integer.parseInt(words[1]);
+                                httprequest.setPort(port);
+                            }
+                            line = reader.readLine();
+                            words = splitter(line, "[ ]+");//drive loop
+                        } else {
+                            uglyword = false;
+                        }
                     }
+                httprequest.addLine(words);
                 }
-                System.out.println(line);
-                httprequest.addLine(line);
+                else{
+                    parsing = false;
+                }
             }
             catch(IOException i){
                 System.out.println(i);
             }
-        } while (!(line.isEmpty()));
-
-
+        } while (parsing);
         //this is where I will trigger the icebox 
         try{
             httprequest.sendRequest();
